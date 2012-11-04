0356074103f8997d618310682d2427096ed8b3f2
Johannes Rosenberger <jo.rosenberger@gmx-topmail.de>
2012-11-04 18:06:01 +0100

    On branch master
     Changes to be committed:
    
    	geändert:   .gitignore
    	  included 'build-list'
    
    	geändert:   build.sh
    	  corrected 'build-list' path

--- build.sh
+++ build.sh
@@ -7,7 +7,7 @@ B_DIR=../build-$RANDOM
 
 mkdir $B_DIR
 
-javac -cp $(ls lib/*) -d $B_DIR $(< build-list)
+javac -cp $(ls lib/*) -d $B_DIR $(< ../build-list)
 
 cd $B_DIR && jar -cf $NAME.jar *
 cp $NAME.jar ../
