0221781deff22c6524ef5880a8dc9c218adb4bf5
Johannes Rosenberger <jo.rosenberger@gmx-topmail.de>
2012-12-21 21:21:17 +0100

    debugged MergedCharacterInputStream via test class
    	moved:   src/ro/jo/java/io/MergedCharacterInputStream.java
    	moved:   src/ro/jo/java/io/tests/MergedCharacterInputStreamTest.java

--- src/ro/jo/java/io/MergedCharacterInputStream.java
+++ src/ro/jo/java/io/MergedCharacterInputStream.java
@@ -1,12 +1,20 @@
-package java.io;
+package ro.jo.java.io;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 import java.util.ArrayList;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class MergedCharacterInputStream extends InputStream {
 
+	public boolean debug;
+	private int debugMsgIndent;
+
 	protected int bufQsize;
-	protected LinkedBlockingQueue<byte[]> bufQueue;
+	public LinkedBlockingQueue<byte[]> bufQueue;
 	protected byte[] buf;
 
 	protected ArrayList<ReadThread> threads;
@@ -16,15 +24,19 @@ public class MergedCharacterInputStream extends InputStream {
 	protected class ReadThread extends Thread {
 
 		protected InputStream in;
+
+		protected ArrayList<ReadThread> threads;
 		protected LinkedBlockingQueue<byte[]> queue;
 
 		protected BufferedReader reader;
 		protected byte[] line;
 		protected int interrupts;
 
-		protected ReadThread (InputStream in, LinkedBlockingQueue<byte[]> queue) {
+		protected ReadThread (InputStream in, LinkedBlockingQueue<byte[]> queue, ArrayList<ReadThread> threads) {
+			threads.add(this);
 			this.in = in;
 			this.queue = queue;
+			this.threads = threads;
 
 			this.reader = new BufferedReader(new InputStreamReader(in));
 			this.line = null;
@@ -32,12 +44,30 @@ public class MergedCharacterInputStream extends InputStream {
 		}
 
 		public void run() {
+			debugMsg("ReadThread.run:", 1);
 			try {
-				while (reader.ready()) {
+				while (!reader.ready())
+					if (debug)
+						System.err.print(".");
+				System.err.print("\n");
+				debugMsg("reader.ready() == " + (reader.ready() ? "true" : "false"), 0);
+
+				boolean read = true;
+				while (reader.ready() && read) {
+					debugMsg("reading lines", 1);
+
 					if (line == null)
-						line = reader.readLine().getBytes();
-					queue.put(line);
-					line = null;
+						line = (reader.readLine() + "\n").getBytes();
+
+					if (line != null) {
+						debugMsg(new String(line), 0);
+						queue.put(line);
+						line = null;
+					} else {
+						read = false;
+					}
+
+					debugMsg("complete", -1);
 				}
 			} catch (IOException e) {
 				e.printStackTrace();
@@ -51,6 +81,8 @@ public class MergedCharacterInputStream extends InputStream {
 
 				e.printStackTrace();
 			}
+			debugMsg("ReadThread.read: complete", -1);
+			threads.remove(this);
 		}
 
 		protected void close() throws IOException {
@@ -64,28 +96,34 @@ public class MergedCharacterInputStream extends InputStream {
 	}*/
 
 	public MergedCharacterInputStream (int queueSize, InputStream[] ins) {
+		debug = false;
+		debugMsgIndent = 0;
+
 		bufQsize = queueSize;
 		bufQueue = new LinkedBlockingQueue<byte[]>(queueSize);
 		threads = new ArrayList<ReadThread>(ins.length);
 
+		closed = false;
+
 		merge(ins);
 	}
 
+	public void setDebug(boolean b) {
+		debug = b;
+	}
 
 	protected void merge(InputStream[] ins) {
 		for (InputStream in : ins) {
-			ReadThread t = new ReadThread(in, bufQueue);
-			threads.add(t);
-			t.run();
+			ReadThread t = new ReadThread(in, bufQueue, threads);
+			t.start();
 		}
 	}
 
 	public int available() throws IOException {
 		if (closed) {
 			throw new IOException("Stream is closed!");
-		} else if ((threads.size() + bufQueue.size()) == 0 && count < buf.length) {
+		} else if ((threads.size() + bufQueue.size()) == 0 && (buf == null || count >= buf.length)) {
 			return 0;
-
 		} else {
 			return 1;
 		}
@@ -105,22 +143,41 @@ public class MergedCharacterInputStream extends InputStream {
 	}
 
 	public int read() throws IOException {
-		if (available() == 0) {
-			return -1;
+		debugMsg("read:", 1);
 
-		} else if (count < buf.length) {
+		int ret;
+
+		if (available() == 0) {
+			debugMsg("available() == 0 -> return -1", 0);
+
+			ret = -1;
+
+		} else if (buf != null && count < buf.length) {
+			debugMsg(String.format("buf available -> return (int) buf[%1$s]", count), 0);
+			ret = (int) buf[count];
+
 			count++;
-			return (int) buf[count];
 
 		} else {
 			try {
+				debugMsg("take buf", 1);
+				debugMsg(bufQueue.toString(), 0);
 				buf = bufQueue.take();
+				debugMsg("complete", -1);
+
+				debugMsg("reset 'count' (count = 0)", 0);
 				count = 0;
-				return read();
+
+				ret = this.read();
+
 			} catch (InterruptedException e) {
+				debugPrintStackTrace(e);
 				throw new IOException("Taking line from the queue failed!");
 			}
 		}
+
+		debugMsg("reading completed", -1);
+		return ret;
 	}
 
 	public int read(byte[] b, int off, int len) throws IOException {
@@ -145,23 +202,52 @@ public class MergedCharacterInputStream extends InputStream {
 	}
 
 	public String readString() throws IOException {
-		int bufSize = (buf.length > 0)
+		debugMsg("readString:", 1);
+
+		int bufSize = (buf != null && buf.length > 0)
 			? buf.length
 			: 10;
+		debugMsg("bufSize: " + bufSize, 0);
+
 
 		ArrayList<Byte> list = new ArrayList<Byte>(bufSize * bufQsize);
-		while (available() > 0) {
-			list.add(Byte.valueOf((byte) read()));
+		debugMsg("read bytes into 'list'", 1);
+
+		for (int i = read(); i > -1; i = read()) {
+			byte b =  (byte) i;
+			list.add(Byte.valueOf(b));
+
+			debugMsg(String.format("read char: '%1$s'", new String(new byte[] { b })), 0);
 		}
+		debugMsg("'ArrayList<Byte> list' readding completed.", -1);
 
 		Byte[] B = list.toArray(new Byte[list.size()]);
 		byte[] b = new byte[B.length];
 
-		for (int i = 0; i > b.length; i++) {
+		for (int i = 0; i < b.length; i++) {
 			b[i] = B[i].byteValue();
 		}
 
 		return new String(b);
+	}
+
+	protected void debugMsg(String msg, int addIndent) {
+		if (debug) {
+			System.err.println(indent(debugMsgIndent) + msg);
+			debugMsgIndent += addIndent;
+		}
+	}
+
+	protected String indent(int num) {
+		String ind = new String();
+		for (int i = 0; i < num; i++)
+			ind += "  ";
+		return ind;
+	}
+
+	protected void debugPrintStackTrace(Exception e) {
+		if (debug)
+			e.printStackTrace();
 	}
 
 }
--- src/ro/jo/java/io/tests/MergedCharacterInputStreamTest.java
+++ src/ro/jo/java/io/tests/MergedCharacterInputStreamTest.java
@@ -1,6 +1,7 @@
-package java.io.tests;
+package ro.jo.java.io.tests;
 
-import java.io.MergedCharacterInputStream;
+import ro.jo.java.io.MergedCharacterInputStream;
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileWriter;
@@ -10,43 +11,54 @@ import java.io.IOException;
 class MergedCharacterInputStreamTest {
 
 	private String[] files;
+	private boolean verbose;
 
-	private class WriteThread {
+	private class WriteThread extends Thread {
 		
 		int filenum;
 		private BufferedWriter b;
 
 		private WriteThread (String file, int filenum) {
-			this.filenum = filenum;
-			b = new BufferedWriter (new FileWriter (file));
-		}
-
-		private String genContent(String line) {
-			for (int i = 1; i <= 7; i++) {
-				content = content + String.format(line, i);
-			}
-		}
-
-		public void run() {
 			try {
-				b.write(genContent(filenum + ": \t line %1$d %n"));
+				this.filenum = filenum;
+				b = new BufferedWriter (new FileWriter (file));
 			} catch (IOException e) {
 				e.printStackTrace();
 				System.exit(1);
 			}
 		}
 
+		private String genContent(String line) {
+			String content = new String();
+			for (int i = 1; i <= 7; i++) {
+				content = content + String.format(line, i);
+			}
+
+			return content;
+		}
+
+		public void run() {
+			try {
+				b.write(genContent(filenum + ": \t line %1$d %n"));
+				b.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+				System.exit(2);
+			}
+		}
+
 	}
 
 
-	private MergedCharacterInputStreamTest (String[] files) {
+	private MergedCharacterInputStreamTest (boolean verbose, String[] files) {
 		this.files = files;
+		this.verbose = verbose;
 		write();
 		read();
 	}
 
 	private void write() {
-		for (int i = 0; i < files; i++) {
+		for (int i = 0; i < files.length; i++) {
 			WriteThread t = new WriteThread (files[i], i);
 			t.start();
 		}
@@ -56,24 +68,41 @@ class MergedCharacterInputStreamTest {
 		try {
 			FileInputStream[] ins = new FileInputStream[files.length];
 
-			for (int i = 0; i < files.length; i++)
+			for (int i = 0; i < files.length; i++) {
 				ins[i] = new FileInputStream (files[i]);
+			}
 
 			MergedCharacterInputStream s = new MergedCharacterInputStream(6, ins);
+			s.setDebug(verbose);
+
+			System.err.println("Test: reading");
 
 			String input = s.readString();
 
-			System.out.println(input);
+			System.out.println("SOT\n" + input + "EOT");
 
 		} catch (IOException e) {
 			e.printStackTrace();
-			System.exit(2);
+			System.exit(3);
 		}
 	}
 
 
 	public static void main(String[] args) {
-		MergedCharacterInputStreamTest test = new MergedCharacterInputStreamTest (args);
+		String[] files;
+		boolean verbose;
+		if (args[0].equals("-v")) {
+			verbose = true;
+			files = new String[args.length - 1];
+			for (int i = 0; i < files.length; i++) {
+				files[i] = args[i + 1];
+			}
+		} else {
+			verbose = false;
+			files = args;
+		}
+
+		MergedCharacterInputStreamTest test = new MergedCharacterInputStreamTest (verbose, files);
 	}
 
 }
